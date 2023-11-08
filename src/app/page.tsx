cfd8d18fc56dda912df5d21adf2729b6dd65b1ae
Mayco Barale <maycobarale@gmail.com>
2023-11-08 16:54:13 -0300

    Update layout of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid h-[100svh] place-content-center p-4">
-      <section className="flex flex-col items-center gap-6 sm:w-[345px]">
+      <section className="flex flex-col items-center gap-8 sm:w-[345px]">
         <div className="flex items-center">
           <Image
             src="/images/whatsapp-shortcut-logo.webp"
@@ -18,12 +18,9 @@ export default function Home() {
             </h1>
           </div>
         </div>
-        <div>
-          <h2 className="text-center text-prussian-blue-900">
-            Envía mensajes de WhatsApp sin necesidad de agregar contactos
-          </h2>
-        </div>
-
+        <h2 className="text-center text-prussian-blue-900">
+          Envía mensajes de WhatsApp sin necesidad de agregar contactos
+        </h2>
         <Form />
       </section>
     </main>
